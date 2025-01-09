0b435aee7f6f185890473a1cc80c48d55c17b068
Bernardo Farah <ber@bernardo.me>
2025-01-09 10:34:13 -0800

    lily58: rules.mk file makeover – reordered semantically

--- keyboards/lily58/rev1/keymaps/berfarah/rules.mk
+++ keyboards/lily58/rev1/keymaps/berfarah/rules.mk
@@ -1,18 +1,27 @@
-LTO_ENABLE = yes            # Link Time Optimization enabled
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+# Basics
 BOOTLOADER = atmel-dfu
-MOUSEKEY_ENABLE = no        # Mouse keys
+LTO_ENABLE = yes            # Reduces FW size
+NKRO_ENABLE = no            # N-Key Rollover, allows for keys to be pressed simultaneously
+
+# Features
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 EXTRAKEY_ENABLE = yes        # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # N-Key Rollover
+
+# Peripherals
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
-SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE = yes           # OLED display
-OS_DETECTION_ENABLE = yes   # https://docs.qmk.fm/features/os_detection
 ENCODER_ENABLE = yes
+OLED_ENABLE = yes           # OLED display
+RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
+
+# Debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+CONSOLE_ENABLE = no         # Console for debug
+
+# Niche features
+AUDIO_ENABLE = no           # Audio output
+MOUSEKEY_ENABLE = no        # Mouse keys
+OS_DETECTION_ENABLE = yes   # https://docs.qmk.fm/features/os_detection
+SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
